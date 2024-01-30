6fe22a14804c187f57809ab561d2c4f5ad932121
枫荷 <fenghe@linkmore.com>
2024-01-30 19:58:38 +0800

    feat(main): basic vue add

--- packages/vue/cdn/app.js
+++ packages/vue/cdn/app.js
@@ -1,19 +1,47 @@
-console.log('hello, vue');
-
 const app = Vue.createApp({
   data() {
     return {
       showBooks: true,
-      title: 'The VueJS Instance',
-      author: 'Max',
-      age: 27
+      url: "https://www.google.com",
+      books: [
+        {
+          title: 'name of the wind',
+          author: 'patrick rothfuss',
+          favor: true,
+        },
+        {
+          title: 'the way of kings',
+          author: 'brandon sanderson',
+          favor: false,
+        },
+        {
+          title: 'the final empire',
+          author: 'brandon sanderson',
+          favor: true,
+        },
+      ]
     };
   },
   methods: {
     toggleShowBooks() {
       this.showBooks = !this.showBooks;
+    },
+    handleEvent(e, data) {
+      console.log('event', e, data);
+    },
+    changeFavor(book) {
+      this.books.forEach(b => {
+        if (b.title === book.title) {
+          b.favor = !b.favor;
+        }
+      });
     }
   },
+  computed: {
+    filteredBooks() {
+      return this.books.filter(book => book.favor);
+    }
+  }
 
 });
 
