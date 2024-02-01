ce5c46be85153c566f19e66d2e62ad51b4218fa9
枫荷 <fenghe@linkmore.com>
2024-02-01 10:39:13 +0800

    feat(main): slot and teleport add

--- packages/vue/modal-project/src/App.vue
+++ packages/vue/modal-project/src/App.vue
@@ -3,21 +3,50 @@
   <h1>{{ msg }}</h1>
   <input type="text" ref="name">
   <button @click="handleClick">click me</button>
-  <Modal />
+  <br>
+  <br>
+  <br>
+  <Teleport to=".modals"  v-if="showModal">
+    <Modal :header="header" :text="text" theme="sale" @close="toggleModal">
+      <p>this is slot children</p>
+      <template v-slot:links>
+        <p>this is a name slot</p>
+      </template>
+    </Modal>
+  </Teleport>
+  <button @click.alt="toggleModal">open Modal (alt)</button>
+
+  <div v-if="showModalTwo">
+    <Modal :header="header" :text="text" @close="toggleModalTwo">
+      <p>this is another slot children</p>
+      <template v-slot:links>
+        <p>this is another name slot</p>
+      </template>
+    </Modal>
+  </div>
+
+  <button @click="toggleModalTwo">open Modal two</button>
+
+
 </template>
 
 
 <script>
+import { Teleport } from 'vue'
 import Modal from './components/Modal.vue'
 export default {
   name: 'App',
   components: {
     Modal,
-    Modal
+    Teleport
 },
   data() {
     return {
-      msg: 'My First Vue 3 App'
+      msg: 'My First Vue 3 App',
+      header: 'Sign up for the Gateway',
+      text: ['text', 'array'],
+      showModal: false,
+      showModalTwo: false,
     }
   },
   methods: {
@@ -25,7 +54,13 @@ export default {
       console.log(this.$refs.name.value)
       this.$refs.name.classList.add('red1')
       this.$refs.name.focus()
-    }
+    },
+    toggleModal() {
+      this.showModal = !this.showModal
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo
+    },
   }
 }
 </script>
--- packages/vue/modal-project/src/components/Modal.vue
+++ packages/vue/modal-project/src/components/Modal.vue
@@ -1,27 +1,54 @@
 <template>
 
-  <div class="backdrop">
-    <div class="modal">
+  <div class="backdrop"  @click.self="closeModal">
+    <div class="modal" :class="{ sale: theme === 'sale' }">
+      <h1>Modal Title</h1>
       <p>modal content</p>
+      <p>{{ header  }}</p>
+      <p>{{  text }}</p>
+      <slot></slot> 
+      <slot>default content</slot>
+      <div class="actions">
+        <slot name="links"></slot>
+      </div>
     </div>
   </div>
 
 </template>
 
-<style>
-.modal {
-  width: 400px;
-  padding: 20px;
-  margin: 100px auto;
-  background: white;
-  border-radius: 10px;
+<script>
+export default {
+  props: ['header', 'text', 'theme'],
+  methods: {
+    closeModal() {
+      this.$emit('close')
+    },
+  },
 }
-.backdrop {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.5);
-}
+</script>
+
+  <style scoped>
+  .modal {
+    width: 400px;
+    padding: 20px;
+    margin: 100px auto;
+    background: white;
+    border-radius: 10px;
+  }
+  .backdrop {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.5);
+  }
+
+  .modal.sale {
+    background:  crimson;
+    color: white;
+  }
+  .modal.modal.sale h1 {
+    color: white ;
+  }
 </style>
