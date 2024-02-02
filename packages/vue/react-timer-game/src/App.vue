64d8dbb45b4e6460574b08e284ff8d10183023ce
枫荷 <fenghe@linkmore.com>
2024-02-02 10:20:05 +0800

    feat(main): vue reaction game add

---
+++ packages/vue/react-timer-game/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <h1>Ninja Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <Results  v-if="showResults" :score="score" />
+</template>
+
+
+<script>
+import Block from './components/Block.vue'
+import Results from './components/Results.vue';
+export default {
+  name: 'App',
+  components: {
+    Block,
+    Results,
+  },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false,
+    }
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      this.showResults = false
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false;
+      this.showResults = true;
+      console.log(reactionTime)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  width: 100%;
+  font-weight: normal;
+  text-align: center;
+  color: #444;
+  margin-top: 120px;
+}
+
+</style>
---
+++ packages/vue/react-timer-game/src/components/Results.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>Reaction Timer - {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ['score'],
+  data() {
+    return {
+      rank: null
+    }
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = 'Ninja Fingers'
+    } else if (this.score < 400) {
+      this.rank = 'Rapid Reflexes'
+    } else {
+      this.rank = 'Snail pace ...'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.rank {
+  font-size: 1.4em;
+  color: aqua;
+}
+</style>
