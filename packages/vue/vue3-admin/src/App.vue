f8fec40f671927d600bbbbc05412af4152a8de69
枫荷 <fenghe@linkmore.com>
2024-02-21 21:07:29 +0800

    feat(main): project init

---
+++ packages/vue/vue3-admin/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header>
+    <el-row class="mb-4">
+      <el-button>Default</el-button>
+      <el-button type="primary">Primary</el-button>
+      <el-button type="success">Success</el-button>
+    </el-row>
+
+    <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+      <div>
+        <div class="text-xl font-medium text-black">ChitChat</div>
+        <p class="text-gray-500">You have a new message!</p>
+      </div>
+    </div>
+  </header>
+
+</template>
