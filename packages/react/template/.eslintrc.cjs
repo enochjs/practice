d74c2442daddae6eb00287eb8bc8b691062b0e2a
enochjs <enochjs@163.com>
2024-03-12 14:11:12 +0800

    feat(main): react template project path alias add

--- packages/react/template/.eslintrc.cjs
+++ packages/react/template/.eslintrc.cjs
@@ -4,51 +4,76 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'plugin:import/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:import/recommended",
   ],
+  // settings: {
+  //   'import/resolver': {
+  //     //   node: {
+  //     //     extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  //     //   },
+  //     alias: {
+  //       map: [['@', './src']],
+  //     },
+  //   },
+  // },
+  settings: {
+    "import/resolver": {
+      node: {
+        paths: ["src"],
+        extensions: [".js", ".jsx", ".ts", ".d.ts", ".tsx"],
+      },
+      typescript: {
+        project: "./tsconfig.json",
+      },
+      alias: {
+        map: [["@", path.resolve(__dirname, "./src")]],
+        extensions: [".js", ".jsx", ".ts", ".d.ts", ".tsx"],
+      },
+    },
+  },
   overrides: [
     {
       env: {
         node: true,
       },
-      files: ['.eslintrc.{js,cjs}'],
+      files: [".eslintrc.{js,cjs}"],
       parserOptions: {
-        sourceType: 'script',
+        sourceType: "script",
       },
     },
   ],
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  settings: { react: { version: '18.2' } },
-  plugins: ['@typescript-eslint', 'react'],
+  settings: { react: { version: "18.2" } },
+  plugins: ["@typescript-eslint", "react"],
   rules: {
-    indent: ['error', 2],
-    'linebreak-style': ['error', 'unix'],
-    quotes: ['error', 'single'],
-    semi: ['error', 'always'],
-    'react/react-in-jsx-scope': 'off',
-    'import/order': [
-      'error',
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "single"],
+    semi: ["error", "always"],
+    "react/react-in-jsx-scope": "off",
+    "import/order": [
+      "error",
       {
         // 对导入模块进行分组
         groups: [
-          'builtin',
-          'external',
-          ['internal', 'parent', 'sibling', 'index', 'object', 'type'],
-          'unknown',
+          "builtin",
+          "external",
+          ["internal", "parent", "sibling", "index", "object", "type"],
+          "unknown",
         ],
-        pathGroupsExcludedImportTypes: ['builtin'],
+        pathGroupsExcludedImportTypes: ["builtin"],
         // newlines-between 不同组之间是否进行换行
         // 'newlines-between': 'always',
         // alphabetize 根据字母顺序对每个组内的顺序进行排序
         alphabetize: {
-          order: 'asc',
+          order: "asc",
           caseInsensitive: true,
         },
       },
