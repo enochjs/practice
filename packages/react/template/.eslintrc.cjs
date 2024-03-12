69d7b4deb0e5cc2b2e5d600e2b74c6a6fa8864fe
enochjs <enochjs@163.com>
2024-03-12 13:57:50 +0800

    feat(main): react template project eslint add

--- packages/react/template/.eslintrc.cjs
+++ packages/react/template/.eslintrc.cjs
@@ -1,18 +1,57 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2021: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:import/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  settings: { react: { version: '18.2' } },
+  plugins: ['@typescript-eslint', 'react'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'react/react-in-jsx-scope': 'off',
+    'import/order': [
+      'error',
+      {
+        // 对导入模块进行分组
+        groups: [
+          'builtin',
+          'external',
+          ['internal', 'parent', 'sibling', 'index', 'object', 'type'],
+          'unknown',
+        ],
+        pathGroupsExcludedImportTypes: ['builtin'],
+        // newlines-between 不同组之间是否进行换行
+        // 'newlines-between': 'always',
+        // alphabetize 根据字母顺序对每个组内的顺序进行排序
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
     ],
   },
-}
+};
--- packages/react/template/src/App.tsx
+++ packages/react/template/src/App.tsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  return (
-    <div className='text-blue-400 font-bold'>template page</div>
-  )
+  return <div className="text-blue-400 font-bold">template page</div>;
 }
 
-export default App
+export default App;
